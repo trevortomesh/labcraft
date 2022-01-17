c41316da63ef4544abcecc5f18702189cde8e1d0
Dustin <dc.christianson@gmail.com>
2022-01-16 19:29:27 -0600

    added some comments re: TODOs for gravity and forces in general

--- forces.py
+++ forces.py
@@ -3,7 +3,8 @@ from ursina import *
 GRAVITY = -9.8
 
 def applyGravity(entity):
-
+    # Checks if entity is currently colliding with anything below itself, if NOT then it applies gravity
     check_for_collision_below = raycast(entity.position, (0, -1, 0), ignore=(entity,), distance=0.5)
     if not check_for_collision_below:
+        # TODO: GRAVITY should work by modulating a veloctiy, not a position..
         entity.y += GRAVITY * time.dt
