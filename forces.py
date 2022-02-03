2a230f67269e5c921828776e3dd9a4fb83db7c95
Dustin <dc.christianson@gmail.com>
2022-02-02 21:19:54 -0600

    updated sims file with gravity code

--- forces.py
+++ forces.py
@@ -1,15 +1,5 @@
 from ursina import *
 
-GRAVITY = (-9.8/2)
-
-def applyGravity(entity):
-    # Checks if entity is currently colliding with anything below itself, if NOT then it applies gravity
-    check_for_collision_below = raycast(entity.position, (0, -1, 0), ignore=(entity,), distance=0.5)
-    if not check_for_collision_below:
-        # TODO: GRAVITY should work by modulating a veloctiy, not a position..
-        entity.y += GRAVITY * time.dt
-
-
 # below function not yet implemented for anything -- the thought is to allow the player ("pusher") to push an object
 def checkForPushing(pusher, pushed):
     check_for_collision_between = raycast(pusher.position, pusher.direction, ignore=(pusher,), distance=0.5)
--- sims.py
+++ sims.py
@@ -1,5 +1,8 @@
 from ursina import *
 
+# Sets a constant value for gravity ( -y ).
+GRAVITY = (-9.8/2)
+
 def oscSim(self):
     self.t += time.dt * self.sliders[6].value
     self.planet.x = (math.cos(self.t) * self.sliders[1].value + self.position.x) * self.sliders[0].value
@@ -12,3 +15,10 @@ def simple_pendulum(self):
     self.pendulum.y = self.position.y
     self.pendulum.z = self.position.z
     self.pendulum.rotation += Vec3(0.5,0,0)
+
+def applyGravity(entity):
+    # Checks if entity is currently colliding with anything below itself, if NOT then it applies gravity
+    check_for_collision_below = raycast(entity.position, (0, -0.65, 0), ignore=(entity,), distance=0.5)
+    if not check_for_collision_below:
+        # TODO: GRAVITY should work by modulating a veloctiy, not a position..
+        entity.y += GRAVITY * time.dt
